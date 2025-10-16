feefb92f50b2a0bd1eda78097e42cafb4d3a9a9c
foxihyun <tlgus0514@naver.com>
2025-10-17 00:02:56 +0900

    5,7주차 과제

---
+++ ch05/sec04/NullPointerExceptionExample.java
@@ -0,0 +1,13 @@
+package ch05.sec04;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		int[] intArray = null;
+		//intArray[0] = 10; ->널포인터익셉션 
+		
+		String str = null;
+
+	}
+
+}
